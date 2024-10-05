5284d14a47f1d3f615c84ae06153a5669ac2a1be
Paolo <paolo.herrera.araya@gmail.com>
2024-10-05 17:26:09 -0300

    Se modifica el boton del carrito para reutilizarlo

--- src/pages/ProductDetailPage/AddToCart.jsx
+++ src/pages/ProductDetailPage/AddToCart.jsx
@@ -1,29 +1,16 @@
-import { motion } from 'framer-motion'
 import { AddShoppingCart } from '@mui/icons-material'
 import { Button } from '@nextui-org/react'
-import PropTypes from 'prop-types'
 
-export default function AddToCart({ onSale, discountPrice, originalPrice }) {
+export default function AddToCart() {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      className="fixed bottom-0 left-0 text-center z-10 p-4 w-full bg-white"
-    >
+    <>
       <Button
         className="color-secondary"
         size="lg"
         startContent={<AddShoppingCart />}
       >
-        Add To Cart - ${onSale ? discountPrice : originalPrice}
+        Add To Cart
       </Button>
-    </motion.div>
+    </>
   )
-}
-
-AddToCart.propTypes = {
-  onSale: PropTypes.bool.isRequired,
-  discountPrice: PropTypes.string,
-  originalPrice: PropTypes.string.isRequired,
 }
