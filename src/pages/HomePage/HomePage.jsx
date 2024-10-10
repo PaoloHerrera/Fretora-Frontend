2e8b5b5d095d3c9559f99a525e1e6b7628535e8f
Paolo <paolo.herrera.araya@gmail.com>
2024-10-10 16:16:11 -0300

    Se modifica la home page agregando 4 cartas después del banner principal y el shop highlights con tabs para cambiar entre categorías

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,20 @@ import video from '../../assets/videos/eko_sa_350.mp4'
 import FeaturedProductVideo from '../../components/FeaturedProductVideo'
 import fretora_reviews from '../../data_testing/fretora.reviews.json'
 import Reviews from '../../components/Reviews'
-import limited_time_sale from '../../data_testing/limited_time.sales.json'
 import offers from '../../data_testing/offers.sales.json'
 import Sales from './Sales'
-import { Divider } from '@nextui-org/react'
+import { Divider, Tabs, Tab } from '@nextui-org/react'
 import top_picks from '../../data_testing/top.picks.json'
 import TopPicks from './TopPicks'
 import OurBrands from './OurBrands'
 import brands from '../../data_testing/fretora.brands.json'
 import BannerContainer from '../../components/BannerContainer'
 import BannerSlide from '../../components/BannerSlide'
+import LimitedTimeSales from './LimitedTimeSales'
+import NewArrivalCard from './NewArrivalCard'
+import BestSellersCard from './BestSelllersCard'
+import ExploreSalesCard from './ExploreSalesCard'
+import ViewAllProductsCard from './ViewAllProductsCard'
 
 export default function HomePage() {
   return (
@@ -37,30 +41,63 @@ export default function HomePage() {
         </BannerContainer>
       </section>
 
+      <section className="grid grid-cols-10 mt-20 gap-8">
+        <div className="col-start-2 col-span-8 sm:col-span-4 sm:col-start-2 lg:col-span-2 lg:col-start-2">
+          <NewArrivalCard />
+        </div>
+        <div className="col-start-2 col-span-8 sm:col-start-6 sm:col-span-4 lg:col-span-2">
+          <BestSellersCard />
+        </div>
+        <div className="col-start-2 col-span-8 sm:col-span-4 sm:col-start-2 lg:col-span-2">
+          <ExploreSalesCard />
+        </div>
+        <div className="col-start-2 col-span-8 sm:col-start-6 sm:col-span-4 lg:col-span-2">
+          <ViewAllProductsCard />
+        </div>
+      </section>
       <section>
         <div className="text-3xl font-bold">
-          <h2 className="text-center pt-10">FRETORA SALES</h2>
+          <h2 className="text-center pt-20 pl-6 pr-6 text-black">
+            SHOP HIGHLIGHTS
+          </h2>
         </div>
-        <Sales limitedTimeSale={limited_time_sale} offers={offers} />
+        <Tabs
+          aria-label="Shop Highlights"
+          className="mt-20 justify-center w-full"
+          color="primary"
+          variant="underlined"
+          classNames={{
+            tabList:
+              'gap-2 md:gap-6 lg:min-w-[1000px] min-w-[100px] rounded-none p-0 border-b border-divider font-bold',
+            cursor: 'w-full bg-[#141496]',
+            tabContent:
+              'group-data-[selected=true]:text-[#141496] text-sm md:text-lg',
+          }}
+        >
+          <Tab key="flash sales" title="Flash Sales">
+            <LimitedTimeSales />
+          </Tab>
+          <Tab key="best offers" title="Best Offers">
+            <Sales offers={offers} />
+          </Tab>
+          <Tab key="our picks" title="Our Picks">
+            <TopPicks products={top_picks} />
+          </Tab>
+        </Tabs>
       </section>
       <section>
-        <div className="text-3xl font-bold pt-10 pl-6">
+        <div className="text-3xl font-bold pt-20 pl-6 pr-6">
           <Divider></Divider>
-          <h2 className="text-center pt-10">FRETORA TOP PICKS</h2>
-        </div>
-        <TopPicks products={top_picks} />
-      </section>
-      <section>
-        <div className="text-3xl font-bold pt-10 pl-6">
-          <Divider></Divider>
-          <h2 className="text-center pt-10">OUR BRANDS</h2>
+          <h2 className="text-center pt-20 text-black">OUR BRANDS</h2>
         </div>
         <OurBrands brands={brands} />
       </section>
       <section>
-        <div className="text-3xl font-bold pt-10 pl-6">
+        <div className="text-3xl font-bold pt-20 pl-6 pr-6">
           <Divider></Divider>
-          <h2 className="text-center pt-10">Feedback from Our Clients</h2>
+          <h2 className="text-center pt-20 text-black">
+            Feedback from Our Clients
+          </h2>
         </div>
         <Reviews reviews={fretora_reviews} />
       </section>
